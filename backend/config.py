923a3dfec4a81b5fddcd05aade09c64c1931fab6
PTA70183292 <alvn.gool@gmail.com>
2026-01-23 20:24:52 +0500

    Refactor Settings class for improved configuration
    Refactor settings configuration to include additional environment variables and update Pydantic settings.

--- backend/config.py
+++ backend/config.py
@@ -1,16 +1,42 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-
-    database_url: str = "postgresql://user:password@db/sentiment_db"
-    base_model_name: str = "DeepPavlov/rubert-base-cased-conversational"
-    model_name: str = "DeepPavlov/rubert-base-cased-conversational"
-    adapter_name: str = "egdfgdfgdsfg/rubert-base-cased-conversational"
+    """
+    Конфигурация приложения.
+    Pydantic автоматически считывает переменные из файла .env,
+    сопоставляя имена полей (case-insensitive) с ключами в файле.
+    """
+    # Переменные должны совпадать с .env (postgres_user -> POSTGRES_USER)
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
     
-    app_title: str = "Sentiment Analysis API"
-    app_version: str = "1.0.0"
-    
-    class Config:
-        env_file = None
+    # Мы берем готовую строку подключения прямо из .env
+    database_url: str 
 
+    base_model_name: str
+    adapter_name: str
+
+    app_title: str
+    app_version: str
+
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+
+    superadmin_email: str
+    manager_email: str 
+
+    default_user_email_1: str 
+    default_user_email_2: str 
+    default_student_email: str
+
+    # Технические настройки Pydantic V2
+    model_config = SettingsConfigDict(
+        env_file=".env",            # Имя файла
+        env_file_encoding="utf-8",  # Кодировка
+        extra="ignore"              # Игнорировать лишние переменные в .env, если они есть
+    )
+
+# Инициализация настроек
 settings = Settings()
